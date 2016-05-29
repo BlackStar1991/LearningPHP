9eceb2111cebccde350ac98397abd259fdcfadeb
Ionov Alexandr Igorevich <alex.dreamsky@gmail.com>
2016-05-29 16:52:04 +0500

    Ready to work with db

--- article.php
+++ article.php
@@ -1,5 +1,5 @@
 <?php
-    require_once("database.php");
+    //require_once("database.php");
     require_once("models/articles.php");
 
     $article = articles_get($_GET['id']);
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-    require_once("database.php");
+    //require_once("database.php");
     require_once("models/articles.php");
-
+    
     $articles = articles_all();
     include("views/articles.php");
 ?>
